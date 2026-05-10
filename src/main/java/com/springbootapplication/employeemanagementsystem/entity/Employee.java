97407342c8036a679b3a4fd2df4483ee5d0831a7
AbhinavBony <abhi.1908sriva@gmail.com>
2026-05-10 17:41:04 +0530

    Entity File updated with generated type as IDENTITY

--- src/main/java/com/springbootapplication/employeemanagementsystem/entity/Employee.java
+++ src/main/java/com/springbootapplication/employeemanagementsystem/entity/Employee.java
@@ -9,7 +9,7 @@ import lombok.*;
 @NoArgsConstructor
 public class Employee {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer Id;
     @Column(name = "Employee_Id", nullable = false, unique = true)
     private Integer empId;
